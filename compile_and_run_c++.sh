855f95e9eb771c54a02b00634c199c4fe8b89f51
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-21 11:55:03 +0200

    Add quit menu option

--- compile_and_run_c++.sh
+++ compile_and_run_c++.sh
@@ -4,16 +4,22 @@
 #
 #
 separator() {
-	printf "\n-----------------------\n"
+	printf "\n------------------------------\n"
 }
 
 displayMenu() {
-	printf "c++ compile and run:\n"
+	printf "c++ compile and run:"
+	separator
 	printf "1 - ls files in current folder"
 	printf "\n2 - compile .cpp file(s)"
 	printf "\n3 - link -o file(s)"
-	printf "\n4 - run exec binary file\n"
+	printf "\n4 - run exec binary file"
+	printf "\nq - quit"
 	separator
+}
+
+invalidChoiceMessage() {
+	printf "\nInvalid choice, please select one of the following:"
 }
 
 # Need to show menu once, then if selection is invalid,
@@ -23,8 +29,8 @@ read userChoice
 
 # Additional "" quoting and separate sets of brackets used for
 # other shells portability.
-while [[ "$userChoice" -lt 1 ]] || [[ "$userChoice" -gt 4 ]]; do
-	printf "\nInvalid choice, please select one of the following:"
+while [[ "$userChoice" != "q" ]] && [[ "$userChoice" -lt 1 || "$userChoice" -gt 4 ]]; do
+	invalidChoiceMessage
 	separator
 	displayMenu
 	read userChoice
@@ -34,24 +40,33 @@ while :; do
 	case $userChoice in
 
 	1)
+		printf "\nFiles in $PWD:"
+		separator
 		ls
 		;;
 	2)
+		separator
 		printf "\nType file(s) to compile: "
 		read fileToCompile
 		;;
 	3)
+		separator
 		printf "\nlinking"
 		;;
 	4)
+		separator
 		printf "\nrunning"
 		;;
+	"q")
+		exit
+		;;
 	*)
+		separator
 		printf "\ninvalid selection\n"
 		;;
 	esac
 
-	[[ ($userChoice -lt 1 && $userChoice -gt 4) ]] || break
+	[[ "$userChoice" -lt 1 ]] || [[ "$userChoice" -gt 4 ]] || break
 done
 
 #printf "\nType executable file name to generate: "
